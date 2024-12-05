1624e80947af8929848a9146482a68967feb00da
rafialfiandro <87708096+rafialfiandro@users.noreply.github.com>
2024-12-05 18:10:30 +0700

    Updated Login Page UI

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', action: function () {
-    return view('login');
+    return view('welcome');
 });
 
 Route::get('/dashboard', function () {
