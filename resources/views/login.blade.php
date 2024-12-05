6db3651db0aa2dca69aaad916ba66863ec9d7d6f
rafialfiandro <87708096+rafialfiandro@users.noreply.github.com>
2024-12-05 09:56:15 +0700

    Updated UI for Login Page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,11 +5,12 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>foodSaver - Login</title>
 </head>
 <body>
-    @extends('templates.footer')
-    @section('content')
+    {{-- @extends('templates.footer') --}}
+    {{-- @section('content') --}}
 
         <div class="container-fluid d-flex flex-column" style="background-color: white; height: 91vh">
             <div class="text-center">
@@ -18,28 +19,72 @@
             </div>
             <div name="loginBox" class="container d-flex flex-column justify-content-center rounded-5 shadow-lg" style="background-color: snow">
                 <p class="fs-1 fw-bold text-center m-4">Login</p>
-                <form>
+
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+
+                    <!-- Email Address -->
                     <div class="mb-3">
                         <div class="container col-4">
-                            <label for="exampleInputEmail1" class="form-label">Email address</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                        </div>
+                            <x-input-label for="email" :value="__('Email Address')" />
+                            <x-text-input id="email" class="form-control" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                        </div>    
                     </div>
+
+                    <!-- Password -->
                     <div class="mb-3">
                         <div class="container col-4">
-                            <label for="exampleInputPassword1" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1">
+                        <x-input-label for="password" :value="__('Password')" />
+
+                        <x-text-input id="password" class="form-control"
+                                    type="password"
+                                    name="password"
+                                    required autocomplete="current-password" />
+
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
                         </div>
                     </div>
+
+                    <!-- Remember Me -->
+                    <div class="mb-4">
+                        <div class="container col-4">
+                        <label for="remember_me" class="inline-flex items-center">
+                            <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
+                            <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+                        </label>
+                        </div>
+                    </div>
+
+                    {{-- Forgot Password  --}}
+                    @if (Route::has('password.request'))
+                        <div class="container col-4 mb-3">
+                        <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                            {{ __('Forgot your password?') }}
+                        </a>
+                        </div>
+                    @endif
+
+                    {{-- Submit Button --}}
                     <div class="container col-2 mb-4">
                         <div class="d-grid gap-2">
-                            <button class="btn btn-success" type="submit">Submit</button>
+                            <x-primary-button class="btn btn-success">
+                                {{ __('Log in') }}
+                            </x-primary-button>
                         </div>
                     </div>
-                </form>
+
+                @if (Route::has('register'))
+                <div class="container col-4 mb-4 text-center">
+                    <a href="{{ route('register') }}">
+                    Don't have an account? Register here
+                    </a>
+                </div>
+                @endif
             </div>
         </div>
 
-    @endsection
+    {{-- @endsection --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', action: function () {
+    return view('login');
 });
 
 Route::get('/dashboard', function () {
