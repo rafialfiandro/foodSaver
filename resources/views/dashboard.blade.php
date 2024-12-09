64478f5ac7ccf7f2dc9fa510ed615c89009b83a9
rafialfiandro <87708096+rafialfiandro@users.noreply.github.com>
2024-12-09 14:28:03 +0700

    Fixed non-existent database bug
    Fixed bug where if a user does not have a table, dashboard will not load

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,41 +12,46 @@
 
                     <h2 class="text-center text-2xl pb-4">Your Food List</h2>
 
-                    <table class="table-auto w-full border-collapse border border-gray-300">
-                        <tbody>
-                        @foreach ($foods as $food)
-                            <tr class="hover:bg-gray-100">
-                                <!-- Food Name column expands to fill remaining space -->
-                                <td class="border border-gray-300 px-4 py-2 w-full">{{ $food->name }}</td>
+                    @if($foods->isEmpty())
+                        <div class="text-center text-gray-600">
+                            <p>No items in the list.</p>
+                        </div>
+                    @else
+                        <table class="table-auto w-full border-collapse border border-gray-300">
+                            <tbody>
+                            @foreach ($foods as $food)
+                                <tr class="hover:bg-gray-100">
+                                    <!-- Food Name column expands to fill remaining space -->
+                                    <td class="border border-gray-300 px-4 py-2 w-full">{{ $food->name }}</td>
 
-                                <!-- Edit button only takes as much space as it needs -->
-                                <td class="border border-gray-300 px-4 py-2 text-center whitespace-nowrap">
-                                    <a href="{{ route('dashboard.foods.edit', $food->id) }}"
-                                       class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-                                    Edit
-                                    </a>
-                                </td>
+                                    <!-- Edit button only takes as much space as it needs -->
+                                    <td class="border border-gray-300 px-4 py-2 text-center whitespace-nowrap">
+                                        <a href="{{ route('dashboard.foods.edit', $food->id) }}"
+                                           class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+                                        Edit
+                                        </a>
+                                    </td>
 
-                                <!-- Delete button only takes as much space as it needs -->
-                                <td class="border border-gray-300 px-4 py-2 text-center whitespace-nowrap">
-                                    <form action="{{ route('dashboard.foods.destroy', $food->id) }}" method="POST" class="inline-block">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit"
-                                                class="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
-                                                onclick="return confirm('Are you sure you want to delete this item?')">
-                                            Delete
-                                        </button>
-                                    </form>
-                                </td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-
+                                    <!-- Delete button only takes as much space as it needs -->
+                                    <td class="border border-gray-300 px-4 py-2 text-center whitespace-nowrap">
+                                        <form action="{{ route('dashboard.foods.destroy', $food->id) }}" method="POST" class="inline-block">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit"
+                                                    class="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
+                                                    onclick="return confirm('Are you sure you want to delete this item?')">
+                                                Delete
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
                     <div class="mt-4">
                         {{ $foods->links() }}
                     </div>
+                    @endif
                     <div class="justify-items-center justify-evenly">
                         <form action="{{ route('dashboard.foods.store') }}" method="POST">
                             @csrf
